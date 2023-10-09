73cf672429a17a1970105e29cb068e63e1011f82
KKH <kkh30123@naver.com>
2023-10-09 18:32:51 +0900

    Modified

--- src/main/java/com/kkh/board/repository/UserRepository.java
+++ src/main/java/com/kkh/board/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kkh.board.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-	// SELECT * FROM user WHERE username = 1?;
+
 	Optional<User> findByUsername(String username);
 
 	User findByEmail(String email);
